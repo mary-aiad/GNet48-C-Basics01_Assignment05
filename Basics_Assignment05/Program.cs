b7aff3a532ec3d38aeb74d8e53268fe6dea0ecdc
mary-aiad <mary.aid.bariz"gmail.com>
2026-08-15 13:43:57 +0200

    Question02

--- Basics_Assignment05/Program.cs
+++ Basics_Assignment05/Program.cs
@@ -3,6 +3,7 @@
     class Book
     {
         private string password = "secret";
+        internal int copiesInStock = 5;
     }
 
     internal class Program
@@ -14,6 +15,12 @@
             ///(outside the class). What happens, and why?
             //Book book = new Book();
             //Console.WriteLine(book.password); // Error because it is private
+            #endregion
+
+            #region Question02
+            ///Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile? Why ?
+            //Book book = new Book();
+            //Console.WriteLine(book.copiesInStock);
             #endregion
         }
     }
