56bcd69c0472c7ffcde35154eabdaf2213d04559
mary-aiad <mary.aid.bariz"gmail.com>
2026-08-15 13:40:09 +0200

    Question01

--- Basics_Assignment05/Program.cs
+++ Basics_Assignment05/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace Basics_Assignment05
 {
+    class Book
+    {
+        private string password = "secret";
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            ///Add a private string password = "secret"; field to a Book class. Try to print it from Main
+            ///(outside the class). What happens, and why?
+            //Book book = new Book();
+            //Console.WriteLine(book.password); // Error because it is private
+            #endregion
         }
     }
 }
